0b696828ae4e2af62bcf8cbd1f96a8163bcd3de9
punit5 <punitpeshwani5@gmail.com>
2018-12-25 20:01:29 +0530

    adding github auth

--- config/prod.js
+++ config/prod.js
@@ -2,12 +2,12 @@
 
 
 module.exports={
-    googleClientID:'process.env.GOOGLE_CLIENT_ID',
-    googleClientSecret:'process.env.GOOGLE_CLIENT_SECRET',
-    mongoURI:'process.env.MONGO_URI',
-    cookieKey:'process.env.COOKIE_KEY',
-    githubClientID:'process.env.GITHUB_CLIENT_ID',
-    githubClientSecret:'process.env.GITHUB_CLIENT_SECRET'
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI,
+    cookieKey:process.env.COOKIE_KEY,
+    githubClientID:process.env.GITHUB_CLIENT_ID,
+    githubClientSecret:process.env.GITHUB_CLIENT_SECRET
     
 };
 
