cc6a4825cfc51ce103de90eecc66a0538eea3234
punit5 <punitpeshwani5@gmail.com>
2018-12-25 07:40:54 +0530

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -4,8 +4,6 @@
 module.exports={
     googleClientID:'process.env.GOOGLE_CLIENT_ID',
     googleClientSecret:'process.env.GOOGLE_CLIENT_SECRET',
-    // githubClientID:'08a61cd9b5bdb181ff4e',
-    // githubClientSecret:'99ccb0752915e9cf24bb1a661c1ac9572c23a423',
     mongoURI:'process.env.MONGO_URI',
     cookieKey:'process.env.COOKIE_KEY'
 };
