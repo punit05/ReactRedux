78af6af9bea35df1beb4b7419ed6477ed3c39efc
punit5 <punitpeshwani5@gmail.com>
2018-12-25 08:19:29 +0530

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -7,3 +7,9 @@ module.exports={
     mongoURI:'process.env.MONGO_URI',
     cookieKey:'process.env.COOKIE_KEY'
 };
+
+
+
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https%3A%2F%2Fthawing-woodland-77529.herokuapp.com%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=process.env.GOOGLE_CLIENT_ID
+
+//mongodb://punit:password5@ds113693.mlab.com:13693/dangit
