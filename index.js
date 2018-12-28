cc7d0c1949b55db70c2fe56323d8faf2fef10fda
punit5 <punitpeshwani5@gmail.com>
2018-12-28 06:19:54 +0530

    ADDED BILLING

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ if(process.env.NODE_ENV==='production')
     app.get('*',(req,res)=>
     {
         res.sendFile(path.resolve(__dirname,'client','build','index.html')
-    )});
+    );
+});
 
 }
 
